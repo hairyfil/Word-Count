39d2e8d6abc7d1e68f927db9f36401248c1a357c
Phil Davidson <phil_davidson@hotmail.com>
2017-06-10 12:06:49 +1200

    Simple Word Count in Python
    Python
    count occurances of words in a text file

---
+++ wordcount.py
@@ -0,0 +1,88 @@
+#
+# Open a file and count how many occurences of each word
+# print the results in a variety of forms
+#
+
+import operator
+import re
+
+file=open("text.txt","r+")
+
+# dictionary is an unordered key/value pair list
+# define the wordcount dictionary to store words in
+wordcount={}
+
+# populate the dictionary with ALL words from the file
+#for word in file.read().split():
+#    if word.lower() not in wordcount:
+#        wordcount[word.lower()] = 1
+#    else:
+#        wordcount[word.lower()] += 1
+
+# populate the dictionary with ALL words from the file
+# all non [a-zA-Z] characters stripped out and changed to lowercase
+
+for word in file.read().split():
+    trackedword = re.sub("[^a-zA-Z]+", "", word.lower())
+    if trackedword not in wordcount:
+        wordcount[trackedword] = 1
+    else:
+        wordcount[trackedword] += 1
+
+# simple print loops
+# Print the dictionary- style #1
+#for w, c in wordcount.items():
+#    print(w, c)
+#
+# Print the dictionary- style #2
+#for w, c in wordcount.items():
+#    print("{}\t{}".format(w, c))
+
+
+print
+print "-----------------------------------------"
+print "Sorted by key - alphabetically"
+print "-----------------------------------------"
+print
+print
+print "Word\t\tCount"
+print "----\t\t-----"
+# long form version of the one actually being used
+# for key, value in sorted(wordcount.items(), key=operator.itemgetter(0)):
+# the key=operator.itemgetter(0) isn't required and is assumed to be the default if omitted
+
+for key, value in sorted(wordcount.items()):
+    if len(key) > 8:
+        print "{}\t{}".format(key, value)
+    else:
+        print "{}\t\t{}".format(key, value)
+
+print
+print "-----------------------------------------"
+print "Sorted by value - small to large"
+print "-----------------------------------------"
+print
+print
+print "Word\t\tCount"
+print "----\t\t-----"
+
+for key, value in sorted(wordcount.items(), key=operator.itemgetter(1)):
+    if len(key) > 8:
+        print "{}\t{}".format(key, value)
+    else:
+        print "{}\t\t{}".format(key, value)
+print
+print "-----------------------------------------"
+print "Sorted by value - large to small"
+print "-----------------------------------------"
+print
+print "Word\t\tCount"
+print "----\t\t-----"
+
+for key, value in sorted(wordcount.items(), key=operator.itemgetter(1), reverse=True):
+    if len(key) > 8:
+        print "{}\t{}".format(key, value)
+    else:
+        print "{}\t\t{}".format(key, value)
+
+file.close();
